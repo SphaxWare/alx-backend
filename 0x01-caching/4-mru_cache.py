6b3f152799a0945cafdea1ace00f3cfff6f88111
Sphax <itssphax123@gmail.com>
2024-06-26 12:16:05 -0700

    4. MRU Caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""MRU Caching"""
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """MRU caching"""
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        Must assign to the dictionary self.cache_data
+        the item value for the key key.
+        """
+        if key is None or item is None:
+            return
+        if key and item:
+            if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                if key in self.cache_data:
+                    self.cache_data[key] = item
+                else:
+                    mru, _ = self.cache_data.popitem(False)
+                    print("DISCARD: {}".format(mru))
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+
+    def get(self, key):
+        """"
+        Must return the value in
+        self.cache_data linked to key
+        """
+        return self.cache_data.get(key, None)
