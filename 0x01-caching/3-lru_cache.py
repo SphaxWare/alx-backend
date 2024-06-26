ff7be9fc27c981a847b74305fba40b98b9fd9135
Sphax <itssphax123@gmail.com>
2024-06-26 11:53:48 -0700

    3. LRU Caching

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""LRU Caching"""
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """LRU caching"""
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """
+        Must assign to the dictionary self.cache_data
+        the item value for the key key.
+        """
+        if key is None or item is None:
+            return
+        if key and item:
+            if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                if key not in self.cache_data:
+                    lru, _ = self.cache_data.popitem(True)
+                    print("DISCARD: {}".format(lru))
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+
+    def get(self, key):
+        """"
+        Must return the value in
+        self.cache_data linked to key
+        """
+        return self.cache_data.get(key, None)
