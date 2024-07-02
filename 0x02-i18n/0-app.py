541bdfced6e551850e5048b825ea4c27549d3903
Sphax <itssphax123@gmail.com>
2024-07-02 09:50:11 -0700

    fix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,15 @@
+#!/usr/bin/env python3
+"""Basic flask app"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """index"""
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
