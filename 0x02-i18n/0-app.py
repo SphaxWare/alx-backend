9bff12ef01165addc22845bc3476afb7ec27e617
Sphax <itssphax123@gmail.com>
2024-07-02 09:56:14 -0700

    fix 2.0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """index"""
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
