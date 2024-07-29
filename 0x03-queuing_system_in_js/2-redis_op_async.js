efb9413f993b76319a4da4aa86f68c29165988c3
= <=>
2024-07-29 20:49:39 +0000

    3. Node Redis client and async operations

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,27 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.message);
+});
+
+
+const setNewSchool = (schoolName, value) => {
+  client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+  const getAsync = promisify(client.get).bind(client);
+  const value = await getAsync(schoolName);
+  console.log(value);
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
