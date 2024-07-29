872b98754f13b500e3e51fec1ce8a954b63fb25f
= <=>
2024-07-29 19:55:42 +0000

    2. Node Redis client and basic operations

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,29 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.message);
+});
+
+const setNewSchool = (schoolName, value) => {
+  client.set(schoolName, value, redis.print);
+}
+
+const displaySchoolValue = (schoolName) => {
+  client.get(schoolName, (err, reply) => {
+    if (err) {
+      console.error(`Error retrieving value for key ${schoolName}: ${err.message}`);
+    } else {
+      console.log(reply);
+    }
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
