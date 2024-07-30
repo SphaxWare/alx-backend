a62e05b033c4ea91ffddce549ef3b143507a6312
= <=>
2024-07-30 16:24:11 +0000

    4. Node Redis client and advanced operations

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,28 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.message);
+});
+
+// Create the hash with the specified values
+client.hset('HolbertonSchools', 'Portland', 50, redis.print);
+client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+client.hset('HolbertonSchools', 'New York', 20, redis.print);
+client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+client.hset('HolbertonSchools', 'Cali', 40, redis.print);
+client.hset('HolbertonSchools', 'Paris', 2, redis.print);
+
+// Retrieve and display the hash
+client.hgetall('HolbertonSchools', (err, result) => {
+  if (err) {
+    console.error('Error retrieving hash:', err);
+  } else {
+    console.log(result);
+  }
+});
